fa931bd02ea229425c246e5325645db785f5e28a
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-03-28 03:28:29 +0300

    Create helloworld-1.c
    1.satır eklendi

---
+++ helloworld-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+main() 
+{
+
+    printf("Merahaba Ben Batuhan");
+    printf("\nBen C dilini gömülü sistemler için öğreniyorum.");
+    printf("Çalışmaya son sürat devam edeceğim");
+    return 0;
+
+}
