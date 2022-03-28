bc0026175344c5f1a5b0acb3cf691c37971c54cf
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-03-28 12:35:57 +0300

    adding some lines and comments
    bazı satırlara yorum ekledim

--- howtoaddcomments.c
+++ howtoaddcomments.c
@@ -1,10 +1,17 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(){
 
 int s1 = 5;
 int s2 = 2;
-return((s1<s2) ? s1:s2);
+return((s1<s2) ? s1:s2); /* Gets the smaller of 2 values */ //how add comments
+
+/* Prints the word Payroll */
+printf("Payroll");
+return 0;
+
+//for (i = 0; i < 25; i++) /* Counts from 0 to 25 */
 
 
 }
