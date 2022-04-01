b0327cf647a91a114f300e5a4b051a795feb259d
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-04-01 16:37:13 +0300

    how to use escape sequences
    I learnt how to use escape sequences

---
+++ printfKullanimi_2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main(){
+
+    /* These three lines show you how to use most popular
+    Escapes Sequences*/
+
+    printf("Column A\tColumn C");
+    printf("\nMy Computer\'s Beep Sounds Like This: \a!\n");
+    printf("\"Letz\bs fix that typo and the show the backslash");
+    printf("character \\\" she said\n");
+    return 0;
+
+}
