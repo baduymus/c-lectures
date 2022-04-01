f2917b9bce2717348b05a61167527c6edf11ad42
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-04-01 16:07:47 +0300

    C-printf Kullanımı
    I learn how write string interger and how to escape sequences

---
+++ printfKullanimi.c
@@ -0,0 +1,47 @@
+/* Batuhan Arslan Joint */
+
+
+
+#include <stdio.h>
+
+main(){
+
+    printf("My favorite number is %d\n",7);
+    //prints my favorite number.
+    /*main() işlevi, programcı tarafından 
+    yazılan programdaki tek işlevdir.*/
+
+    // Printing Strings
+
+    printf("I am on my way to C mastery\n");
+
+    printf("Write Code");
+
+    printf("Learn C\a");
+
+
+    /*
+        \n
+        \a
+        \b
+        \t
+        \\
+        \'
+        \* 
+
+    */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
