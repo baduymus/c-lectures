0f0c68d68fabbf9cbc09d7270061501eedb4a3f2
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-04-03 21:59:13 +0300

    Update howtoaddcomments.c

--- howtoaddcomments.c
+++ howtoaddcomments.c
@@ -8,7 +8,7 @@ int s2 = 2;
 return((s1<s2) ? s1:s2); /* Gets the smaller of 2 values */ //how add comments
 
 /* Prints the word Payroll */
-printf("Payroll");
+printf("Payroll deneme 123");
 return 0;
 
 //for (i = 0; i < 25; i++) /* Counts from 0 to 25 */
