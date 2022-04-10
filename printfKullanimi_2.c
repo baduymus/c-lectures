80dbd485793d978beba33b91f9c32a02cabc5285
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-04-10 15:29:23 +0300

    deneme-yapıldı

--- printfKullanimi_2.c
+++ printfKullanimi_2.c
@@ -9,6 +9,14 @@ main(){
     printf("\nMy Computer\'s Beep Sounds Like This: \a!\n");
     printf("\"Letz\bs fix that typo and the show the backslash");
     printf("character \\\" she said\n");
+    print("merhehahaha");
+
+
+
+    
+    print("Deneme_1_2_^3");
     return 0;
+
+
 
 }
