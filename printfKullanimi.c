71d9ec1c79abf49c0c63d0c1e4faa4b263db703e
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-04-18 15:23:39 +0300

    Update printfKullanimi.c

--- printfKullanimi.c
+++ printfKullanimi.c
@@ -1,49 +1,9 @@
-/* Batuhan Arslan Joint */
+#include<stdio.h>
+#include<stdlib.h>
 
+main() {
 
-
-#include <stdio.h>
-
-main(){
-
-    printf("My favorite number is %d\n",7);
-    //prints my favorite number.
-    /*main() işlevi, programcı tarafından 
-    yazılan programdaki tek işlevdir.*/
-
-    // Printing Strings
-
-    printf("I am on my way to C mastery\n");
-
-    printf("Write Code");
-
-    printf("Learn C\n");
-
-
-        // ESCAPE SEQUENCES
-
-    /*
-        \n
-        \a
-        \b
-        \t
-        \\
-        \'
-        \* 
-
-    */
-
-
-    /*The \n could have been placed at the beginning of the second
-        line, and the same output would have occurred. */
-
-
-    printf("Write code\nLearn C");
-
-
-
-
-
+    printf("Hello");
 
 
 }
