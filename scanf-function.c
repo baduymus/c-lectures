d9d93f2811751655f2c5ffd9fa150c945df6c949
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-04-19 06:47:54 +0300

    scanf fonksiyonu dersi
    Burada scanf fonksiyonun nasıl çalıştıığını ve kullanıcıdan nasıl veri alacağımı öğrendim. Eger bir şeyi ekrana yazdırmak istiyorsam "%s" kısaltmasını kullanmayı unutmam  gerekiyor. ve doğru yazım şekli scanf("%s",degiskenadi); olarak

---
+++ scanf-function.c
@@ -0,0 +1,57 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(){
+
+    /*char kitapad[20] = "Son Ada";
+    printf("Kitap Adi: %s",kitapad);*/
+
+
+    /*scanf kullanıcının veri girmesi gerektiği durumlarda kullanılyıor
+    mesela diktörgenin alanını veya çevresini bulurken kullanıcının
+    değerlerini almamız gerekiyor hesaplayabilmemiz için*/
+
+    /*char kitapad[30];
+    printf("Kitabin Adini Giriniz: ");
+    scanf("%s",kitapad);*/
+    //printf("Girmis olduğunuz kitap: %s",kitapad);
+    /* Scanfde şöyle bir dezavantaj var scanf boşluktan sonraki değerleri almıyor.
+    Tek kelime veriler için uygun olabilir fakat birden fazla
+    giriş değeri verdiğimizde*/
+
+    //Bir Personelin bilgilerini tutan bir menü yapıyorum.
+
+    char Ad[10],Soyad[10],Yas[2],Sehir[13],Meslek[8];
+    printf("Adiniz: ");
+    scanf("%s",Ad);
+
+    printf("Soyadiniz: ");
+    scanf("%s", Soyad);
+
+    printf("Yasiniz: ");
+    scanf("%s",Yas);
+
+    printf("Sehiriniz: ");
+    scanf("%s",Sehir);
+
+    printf("Mesleginiz: ");
+    scanf("%s",Meslek);
+
+    printf("\n");
+
+    printf("Adi: %s - Soyadi: %s\n",Ad,Soyad);
+    printf("Yasiniz: %s\n",Yas);
+    printf("Meslek: %s - Sehir: %s\n",Meslek,Sehir);
+
+
+    return 0;
+
+    // Ödev 
+    /*kitap Adı
+    yazari
+    sayfa sayısı
+    basim tarihi
+    basim evi
+    tür 
+    olarak bir menü oluşturacağım*/
+}
