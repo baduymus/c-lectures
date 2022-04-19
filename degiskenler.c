2e827cc34f9a3720536c9c305d73ba7efe7a1c6f
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-04-19 06:15:44 +0300

    degiskenler char veri tipi
    char veri tipinde c dillinde nasıl string degerleri tutuğunu öğrendim. Her bir karakteri kendi içerisinde tutuyor yani = BURSA şehrini B-U-R-S-A olarak harf harf tutuyor.

--- degiskenler.c
+++ degiskenler.c
@@ -1,5 +1,5 @@
-//değişken programda kullanacağımız verileri komutlarla kullanacağımız değerlerdir.
-// algoritme doğru şeyi doğru yerde doğru zamanda kullanmaktır.
+/*değişken programda kullanacağımız verileri komutlarla kullanacağımız değerlerdir.
+ algoritme doğru şeyi doğru yerde doğru zamanda kullanmaktır.*/
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -12,3 +12,30 @@
     long en büyük tam sayı değerini
     short küçük tam sayı değeri
     */
+
+   int main(){
+
+       /*char kitapadi[20] = "Mufettis";
+       char kitapyazar[10] = "Gogol";
+    //C dilinde yazdırma isleminde "%s" özel bir komut kullanıyoruz.
+    //Buradaki s -scannerdan geliyor scan anlamı ekranda göster anlamında kullanıyoruz.
+    printf("%s\n",kitapadi);
+    printf("%s",kitapyazar);*/
+
+    /*char kitaptur[10] = "Tiyatro kitabi";
+    printf("Kitapturu : %s",kitaptur);*/
+
+    char kitapadi[21] = "Avucunuzdaki kelebek";
+    char yazar[30] = "Ahmet Serif Izgoren";
+    char turu[10] = "Hikaye";
+    char sayfa[3] = "124";
+    char basimyil[4] = "2001";
+
+    printf("******** Kitap Tanitim ********\n\n");
+    printf("Kitap adi: %s\n",kitapadi);
+    printf("Yazar: %s\n",yazar);
+    printf("Turu: %s\n",turu);
+    printf("Sayfa Sayisi: %s\n",sayfa);
+    printf("Basim Yili: %s\n",basimyil);
+
+   }
