bac78114e6c38ffaf46d24bca6d1791c939357ce
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-04-19 03:56:05 +0300

    Create degiskenler.c

---
+++ degiskenler.c
@@ -0,0 +1,14 @@
+//değişken programda kullanacağımız verileri komutlarla kullanacağımız değerlerdir.
+// algoritme doğru şeyi doğru yerde doğru zamanda kullanmaktır.
+
+#include<stdio.h>
+#include<stdlib.h>
+
+/* string harf ve metinler için kullanılan değişkendir
+    int tam sayılar için kullanılır
+    double ondalık sayıları için
+    float
+    char 
+    long en büyük tam sayı değerini
+    short küçük tam sayı değeri
+    */
