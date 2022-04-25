3547f4f6fb4e51cbfd44eeb3cf06bbe842f493ef
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-04-25 23:56:18 +0300

    sadiseker_dersleri_1

--- degiskenler_2.c
+++ degiskenler_2.c
@@ -12,15 +12,28 @@
 
 int main(){
 
-    int a = 10;
+    /*int a = 10;
     int b = 3, c = 4;
-    int d = 13;
+    int d = 13;*/
     int x;
 
-    printf("a : %d, b : %d, c : %d",a,b,c);
-    printf(" d : %d",d);
+   // printf("a : %d, b : %d, c : %d",a,b,c);
+   // printf(" d : %d",d);
+
+    printf("Lutfen bir sayi giriniz:");
+    scanf("%d",&x);
+    printf("Girilen Sayi : %d\n",x);
+    float pi = 3.14;
+    printf("%8.4f",pi);
 
     //%d tırnak bitikten sonra ben bir değişken koyacam al o değişkeni %d den önceki değere koy
 
+    //Özel sembollerin ekrana basılması
+    /* \n
+        \t
+        \b
+        \\
+        \"*/
+        printf("\n\\");
 
 }
