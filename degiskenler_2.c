8db81712c05aef0cd0a3f99cef4c14f3af6418d9
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-04-25 11:22:15 +0300

    Degisken Tanımlama

---
+++ degiskenler_2.c
@@ -0,0 +1,26 @@
+   //Değişkenler
+    
+    /*Değişken İsmş
+    Değişkenin Tipi
+    Hafızadaki Yeri
+    Değişkenin Değeri*/
+
+    //int,float ,double,char
+
+    //C dilindeki Değişken Kullanımı
+#include<stdio.h>
+
+int main(){
+
+    int a = 10;
+    int b = 3, c = 4;
+    int d = 13;
+    int x;
+
+    printf("a : %d, b : %d, c : %d",a,b,c);
+    printf(" d : %d",d);
+
+    //%d tırnak bitikten sonra ben bir değişken koyacam al o değişkeni %d den önceki değere koy
+
+
+}
