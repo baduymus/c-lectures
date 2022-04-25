ad2f0fda1f6f3174b6bde4eb9ead1700ca9479a4
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-04-25 11:22:04 +0300

    İsim Yazdırma

--- printfKullanimi.c
+++ printfKullanimi.c
@@ -1,12 +1,14 @@
 #include<stdio.h> //standart libary
-#include<stdlib.h>
 
 int main() { /*main fonksiyonu işletim 
 sisteminden çağırılan özel bir fonksiyondur*/
 
+//main fonksiyonu her yazdığımızda olması gerekiyor.
 
-    printf("Hello");
+    printf("Batuhan Arslan");
 
     return 0;
+
+
 
 }
