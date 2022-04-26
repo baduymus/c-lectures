b53f63bf3fd6adbfce8b00cb360a8577c24ba1ca
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-04-26 23:38:41 +0300

    conditions

---
+++ lectures/FlowControl.c
@@ -0,0 +1,37 @@
+//Koşullar(Conditions)
+
+#include<stdio.h>
+int main(){
+
+    /*int i;
+    scanf("%d",&i);
+
+    if(i>5)
+    {
+        printf("Buyuk\n");
+    }
+    else
+    {
+        printf("Kucuktur\n");
+    }
+    printf("Bitti");
+    
+    return 0;*/
+
+    int a,b;
+
+    scanf("%d\n%d",&a,&b);
+    
+    if(a>b){
+        printf("A B'den buyuktur: ");
+        
+    }
+    else{
+        printf("B A'dan buyuktur ");
+    }
+
+
+}
+    
+
+
