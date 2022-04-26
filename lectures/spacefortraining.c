18184d6e1149ca989f40cd051ce76d791a962580
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-04-26 18:45:11 +0300

    training

---
+++ lectures/spacefortraining.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+
+    float pi = 3.14;
+    int a;
+    printf("%d\n",(int)pi); //pi değerini floatdan int dönüştürme
+    printf("%f\n",pi);
+    a = pi;
+    printf("%d",a);
+    a = 2.15*3;
+    pi = 2.15*3;
+    printf("%d %f",a,pi);
+    
+
+}
