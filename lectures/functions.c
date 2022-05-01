107e988803f3fd98637465df00bf468516d2dc0d
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-05-01 08:07:27 +0300

    fonksiyonlar

--- lectures/functions.c
+++ lectures/functions.c
@@ -1,19 +1,28 @@
 #include<stdio.h>
 
 
-int main(){
+/*int main(){
     int x;
     printf("Bir sayi giriniz : ");
     scanf("%d",&x);
     printf("Sonuc = %d",fact(x));
 
-}
+} */
 
-int fact(int x){
+/*int fact(int x){
     int sonuc = 1;
 
     for (int i = 1; i <=x; i++){
             sonuc *=i;
     }
-    return sonuc;
-}
+    return sonuc;*/
+
+
+    //özyineli fonksiyon("Recursive")
+
+    int f(int n){
+        if(n==0)
+            return 1;
+            return n*f(n-1);
+    }
+    
