583dcdf3f0d0bfee1efae52ed76317f1466b3f39
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-05-01 08:07:36 +0300

    sonsuz dongu calismam

---
+++ lectures/sonsuzdongu.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+
+int main(){
+    /*int i = 1;
+    while(i<=10){
+
+    printf("%d\n",i);
+    i++;
+    if(i == 5){
+
+        break;
+    }
+        
+    }*/
+
+    /*for(int i = 1; i<=10;i++){
+
+        for(int j = 1; j<=10;j++){
+            
+            printf("%d\t",i*j);
+
+        }
+
+        printf("\n");
+
+    }*/
+
+    /*for(int i = 1; i<=5;i++){
+        for(int j =1; j<=5;j++){
+            if((i+j)%2 == 0){
+
+            printf("*");
+
+            }
+
+            else
+            printf(" ");
+
+        }
+
+        printf("\n");
+
+    }*/
+
+    /*for (int i = 0; i < 10; i++){
+        for (int j = 0; j <= 1; j++){
+
+            int nf=1;
+            for (int p = 0; i < 10; i++){
+                
+
+            }
+            
+
+        }*/
+        
+        
+
+        
+    }
+    
+
+
+}
