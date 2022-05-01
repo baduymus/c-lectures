a33a98633e537a5935a9c461b2b29bc32009fdfc
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-05-01 11:57:12 +0300

    dizinin elemanlarını değerlerini ekrana yazdırma.

--- lectures/diziler_Arrays.c
+++ lectures/diziler_Arrays.c
@@ -5,16 +5,35 @@
 
 int main(){
 
-    int idizi[10];
+    /*int idizi[10];
     idizi[0] = 21;
     idizi[1] = 35;
     idizi[2] = 34;
 
 
-    printf("%d" ,idizi[0]);
-    printf("%d ",idizi[1]);
-    printf("%d ",idizi[2]);
+    printf("1. Index =  %d " ,idizi[0]);
+    printf("2. Index = %d ",idizi[1]);
+    printf("3. Index  = %d ",idizi[2]);
 
-    return 0;
+    return 0;*/
+
+
+    int idizi[10];
+    int id;
+    // Dizinin elemanlarının 1-10 arasındaki değerleri sıra ile atama 
+    for (id = 0; id <10; id++)
+    {
+        idizi[id] = id +1;
+    }
+
+    //Dizinin değerlerini ekrana yazdırmak.
+    for (id =0; id<10; id++)
+    {
+        printf("%d ", idizi[id]);
+    }
+    
+    
+
+
 
 }
