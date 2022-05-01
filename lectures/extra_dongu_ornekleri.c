01c2145d2a3fe0bef30842953bac43abdaec599d
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-05-01 08:07:17 +0300

    dongu ornekleri

---
+++ lectures/extra_dongu_ornekleri.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+int main(){
+    /*int i;
+    for(i = 0; i<10; i++){
+        printf("%d Batuhan Arslan\n",i);
+    }*/
+
+
+    /*for(int i= 100; i>=50;i--){
+        if(i%3==0){
+            printf("%d\n",i);
+        }
+           
+    }*/
+
+
+    /*int i = 1;
+    while (i<=20)
+    {
+        
+        if(i%10==0){
+            printf("%d",i);
+            i--;
+        }
+        
+    }*/
+
+    /*for (int i = 0; i < 5; i++)
+    {
+        printf("%d\n",100-1-i*3);
+
+
+    }*/
+
+
+    //faktoriyeli hesaplanması istediiginiz sayiyi giriniz.
+
+    
+
+    
+    
+
+    
+}
