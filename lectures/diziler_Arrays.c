082ba1c56c9015a24034d647c194842eb0f1d527
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-05-01 11:45:49 +0300

    array_alistirma

--- lectures/diziler_Arrays.c
+++ lectures/diziler_Arrays.c
@@ -1,10 +1,20 @@
+/* Burada dizileri anlamak için bir kaç
+örnek yaptım*/
+
 #include<stdio.h>
 
 int main(){
 
-int x;
-scanf("%i", &x);
-printf("You entered the value %i\n",x);
+    int idizi[10];
+    idizi[0] = 21;
+    idizi[1] = 35;
+    idizi[2] = 34;
 
+
+    printf("%d" ,idizi[0]);
+    printf("%d ",idizi[1]);
+    printf("%d ",idizi[2]);
+
+    return 0;
 
 }
