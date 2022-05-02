ebff179ad07c2c4842fa79f62a8af87e867ed132
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-05-02 16:35:00 +0300

    pointers

---
+++ lectures/pointers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+
+    int a=10;
+    int *p;
+
+    p = &a;
+
+    printf("%d\n",*p); // p'nin gösterdiği yeri basar
+    printf("%d\n",p); //p'nin değerini yani p'nin gösterdiği yerin
+    //adresini basar
+
+    printf("%d\n",a); // a'nın adresini basar
+    printf("%d\n",&a); // a'nın adresini basar
+    printf("%d\n",&p);// p'nin adresini basar
+
+    return 0;
+
+
+
+
+}
