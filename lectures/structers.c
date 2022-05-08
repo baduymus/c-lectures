caa3d2863c950206da04a533d1a6660b03db7faf
Batuhan ARSLAN <arslanbatu97@gmail.com>
2022-05-08 11:52:49 +0300

    struct

--- lectures/structers.c
+++ lectures/structers.c
@@ -1,55 +1,92 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+
+/*
+Yapı değişkenlerini oluşturabilmemiz için önce veri tipini
+tanımlamamız gerekir. Bir yapı tanımlamak için struct anahtar sözcüğü kullanılır.
+
+*typedef bir tip tanımlamaya yarar.
+
+aşağıda bir insan yapısı tanımladım
+insanın yapısının içerisindeki belirlenmis olan degiskenler
+insan yapısının degiskenleri yani nasıl oluyorsa bir int veya double degiskeni gibi
+insan yapısının içerisindeki örnegin yas degiskeni insan yapısının içerisinde tanımlanmıştır.
+bu yas integer olarak tanımlı bir kendi degiskenimizi olustururken enum dan yararlanıyoruz.
+
+enumaretor yani 'sayısı' veya 'numaralandırıcı' dan geliyor
+
+bir insanın 2 olasılıklı cinsiyeti vardır.
+
+bay veya bayan bunlar bize dönen değer 0 veya 1 
+bay ise == 0
+bayan ise == 1
+
+insan yapısının içerisinde belirlemis oldugumuz cinsiyet tipini cagiriyoruz.
+
+structları pointer olarakda kullanabiliriz.
+
+
+
+*/
 typedef enum{
 
-    bay,bayan
+    bay,//0
+    bayan //1
 
 }cinsiyet;
 
-typedef enum
-{
-    pazartesi,//0
-    sali, //1 
-    carsamba, // 2
-    persembe, // 3
-    cuma,
-    cumartesi,
-    pazar
-}gunler;
 
 typedef struct {
     int yas;
     char *isim;
     cinsiyet c;
-    gunler tatilgunu;
 
 } insan;
 
 
+int emeklimi(insan *birey){
+
+    if(birey->c == bay && birey->yas > 55 ){
+        return 1;
+    }
+
+    if(birey->c == bayan && birey->yas >50){
+        return 1;
+    }
+    return 0;
+}
+
 int main(){
 
-    char **gunler;
+    
 
-    gunler[0] = 'pazartesi';
-    gunler[1] = 'sali';
-    gunler[2] = 'carsamba';
-    gunler[3] = 'persembe';
-    gunler[4] = 'cuma';
     
-    insan batu;
-    
-    batu.tatilgunu = cuma;
-    printf("batu TATIL GUNU : %u",batu.tatilgunu);
+    /*batu degiskeni bir oointer olduğu için öncelikle bunun
+    içerisine bir deger atamadan önce malloc memorial location yapmak
+    gerekiyor
+
+    degisken bir // struct pointeriysa 
+    */
+
 
     int a;
     insan ali;
+    insan *batu;
+    batu = (insan*)malloc(sizeof(insan));
+    batu -> yas = 24;
     ali.yas = 30;
     ali.c = bay;
-    ali.tatilgunu = persembe;
+    printf("\nBatunun yasi: %d",batu->yas);
     printf("\nAlinin yasi : %d",ali.yas);
     printf("\nAlinin cinsiyeti : %u",ali.c);
-    //printf("\nAlinin tatil Gunu : %u",[ali.tatilgunu]);
+
+
+    printf("\nbatu emekli mi :%d",emeklimi(&batu));
+
+    printf("\nali emekli mi  :%d",emeklimi(&ali));
+
+    
 
 
 }
